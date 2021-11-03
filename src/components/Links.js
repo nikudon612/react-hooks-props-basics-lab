da5986aa9e4197381267a27bb5dfacaf9d718f44
Nick Bechtel <bechtel.nick@gmail.com>
2021-11-03 10:40:05 -0400

    commit tree 8af80f80c8cb827e52a97b7c1238216a17ce9d38

---
+++ src/components/Links.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Links(props) {
+  return (
+    <div id="linksId">
+      <h3>Links</h3>
+      <a href="https://github.com/liza">https://github.com/liza</a>
+      <a href="https://www.linkedin.com/in/liza">
+        https://www.linkedin.com/in/liza/
+      </a>
+    </div>
+  );
+}
+
+export default Links;
